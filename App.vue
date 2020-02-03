4d21036cb81e0440fe6735046f896841a697cb0d
rafiihammam <43406179+rafiihammam@users.noreply.github.com>
2020-02-03 21:57:46 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1 :class="'notActive'">{{name}}</h1>
+    <h1 :class="'active'">{{name}}</h1>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="hello world"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/kalkulator.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
